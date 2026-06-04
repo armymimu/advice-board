ddae27bea2250f2730e7a5051a494456d4e9cb05
[armymimu-ocr] <[ooaz1111@gmail.com]>
2026-06-05 02:00:46 +0700

    Explicitly set executablePath for puppeteer to ensure it uses the system Chromium

--- server.js
+++ server.js
@@ -75,6 +75,7 @@ async function autoRefreshToken() {
   try {
     browser = await puppeteer.launch({
       headless: true,
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
       args: [
         '--no-sandbox', 
         '--disable-setuid-sandbox', 
